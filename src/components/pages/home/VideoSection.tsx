30cb9eef314632c8c2328fd8435c1e7c78f3396b
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2023-09-16 12:18:15 -0600

    video section & svgs fix

---
+++ src/components/pages/home/VideoSection.tsx
@@ -0,0 +1,52 @@
+import Preferences from '@/public/svgs/preferences.svg';
+import Play from '@/public/svgs/play.svg';
+import { twMerge as tw } from 'tailwind-merge';
+import Image from 'next/image';
+
+const PlayButton = () => (
+  <button
+    type='button'
+    className={tw(
+      `w-64 lg:w-auto absolute top-full left-1/2 flex items-center transform
+      -translate-y-1/2 -translate-x-1/2 bg-white rounded-full font-medium group p-4 shadow-xl`
+    )}
+    aria-label='play video'
+  >
+    <Image
+      src={Play}
+      alt='play'
+      width={24}
+      height={24}
+      className={tw(
+        `w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 flex-shrink-0`
+      )}
+    />
+    <span className={tw(`ml-3`)}>Watch the video (5 min)</span>
+  </button>
+);
+
+const VideoSection = () => (
+  <section
+    className={tw(`bg-gradient-to-b from-gray-50 to-white shadow-inner`)}
+  >
+    <div className={tw(`max-w-7xl mx-auto`)}>
+      <div className={tw(`flex flex-col max-w-4xl mx-auto pt-28`)}>
+        <div className={tw(`w-full`)}>
+          <div className={tw(`relative shadow-2xl mx-6 lg:mx-0`)}>
+            <Image
+              src={Preferences}
+              alt='preferences'
+              width={1280}
+              height={720}
+              className={tw(`rounded-lg`)}
+              priority
+            />
+            <PlayButton />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default VideoSection;
